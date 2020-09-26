25b64e8749c3f6b246fb9cdb58cd354e15f729cf
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-26 05:44:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,14 +26,14 @@ def game_hash
          slam_dunks: 7
         },
         {player_name: 'Brook Lopez', 
-        number: 11, 
-        shoe: 17, 
-        points: 17, 
-        rebounds: 19, 
-        assists: 10, 
-        steals: 3, 
-        blocks: 1, 
-        slam_dunks: 15
+          number: 11, 
+          shoe: 17, 
+          points: 17, 
+          rebounds: 19, 
+          assists: 10, 
+          steals: 3, 
+          blocks: 1, 
+          slam_dunks: 15
         },
         {player_name: 'Mason Plumlee', 
         number: 1, 
