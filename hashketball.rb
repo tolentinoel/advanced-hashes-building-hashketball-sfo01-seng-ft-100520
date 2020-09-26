3fd6c114db100fcbe8ac435b0c6879ee6604aa62
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-26 05:43:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,7 +60,7 @@ def game_hash
       
     :away => {
       :team_name => 'Charlotte Hornets',
-      :colors => ['turquoise', 'purple'],
+      :colors => ['Turquoise', 'Purple'],
       :players => [
         {player_name: 'Jeff Adrien', 
         number: 4, 
